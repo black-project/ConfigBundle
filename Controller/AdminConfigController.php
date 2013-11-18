99e8243b28edb21785733560fa37f446e8f2a6a9
Alexandre pocky Balmes <albalmes@gmail.com>
2013-11-19 00:41:09 +0100

    Début du refactoring

--- Controller/AdminConfigController.php
+++ Controller/AdminConfigController.php
@@ -11,26 +11,196 @@
 
 namespace Black\Bundle\ConfigBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Form\Extension\Csrf\CsrfProvider\CsrfProviderInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use JMS\SecurityExtraBundle\Annotation\Secure;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Black\Bundle\ConfigBundle\Form\Type;
-use Symfony\Component\Serializer\Exception;
 use Black\Bundle\ConfigBundle\Exception\ConfigNotFoundException;
+use Black÷Bundle\ConfigBundle\Form\Handler\ConfigFormHandler;
+use Symfony\Bundle\FrameworkBundle\Routing\Router;
 
 /**
  * Class AdminConfigController
  *
- * @Route("/admin/config")
+ * @Route("/admin/config", service="black_config.controller.admin_config")
  *
  * @package Black\Bundle\ConfigBundle\Controller
  * @author  Alexandre Balmes <albalmes@gmail.com>
  * @license http://opensource.org/licenses/mit-license.php MIT
  */
-class AdminConfigController extends Controller
+class AdminConfigController implements ConfigControllerInterface
 {
+    /**
+     * @var ConfigManagerInterface
+     */
+    protected $configManager;
+    /**
+     * @var \Symfony\Component\Form\Extension\Csrf\CsrfProvider\CsrfProviderInterface
+     */
+    protected $csrf;
+    /**
+     * @var \Black÷Bundle\ConfigBundle\Form\Handler\ConfigFormHandler
+     */
+    protected $handler;
+    /**
+     * @var \Symfony\Component\HttpFoundation\Request
+     */
+    protected $request;
+    /**
+     * @var \Symfony\Bundle\FrameworkBundle\Routing\Router
+     */
+    protected $router;
+    /**
+     * @var \Symfony\Component\HttpFoundation\Session\SessionInterface
+     */
+    protected $session;
+
+    /**
+     * @param ConfigManagerInterface $configManager
+     * @param ConfigFormHandler      $handler
+     * @param Request                $request
+     * @param CsrfProviderInterface  $csrf
+     * @param SessionInterface       $session
+     * @param Router                 $router
+     */
+    public function __construct(
+        ConfigManagerInterface $configManager,
+        ConfigFormHandler $handler,
+        Request $request,
+        CsrfProviderInterface $csrf,
+        SessionInterface $session,
+        Router $router
+    )
+    {
+        $this->configManager    = $configManager;
+        $this->handler          = $handler;
+        $this->request          = $request;
+        $this->csrf             = $csrf;
+        $this->session          = $session;
+        $this->router           = $router;
+    }
+
+    /**
+     * @Method({"POST"})
+     * @Route("/config/batch", name="admin_config_batch")
+     *
+     * @return array
+     *
+     * @throws \Symfony\Component\Serializer\Exception\InvalidArgumentException If method does not exist
+     */
+    public function batchAction()
+    {
+        $request    = $this->request;
+        $flashBag   = $this->session->getFlashBag();
+
+        $token      = $this->csrf->isCsrfTokenValid('batch', $request->get('token'));
+
+        if (!$ids = $request->get('ids')) {
+            $flashBag->add('error', 'black.bundle.config.error.config.admin.config.no.item');
+
+            return new RedirectResponse($this->generateUrl('admin_config'));
+        }
+
+        if (!$action = $request->get('batchAction')) {
+            $flashBag->add('error', 'black.bundle.config.error.config.admin.config.no.action');
+
+            return new RedirectResponse($this->generateUrl('admin_config'));
+        }
+
+        if (!method_exists($this, $method = $action . 'Action')) {
+            throw new \Exception(
+                sprintf('You must create a "%s" method for action "%s"', $method, $action)
+            );
+        }
+
+        if (false === $token) {
+            $flashBag->add('error', 'black.bundle.config.error.config.admin.config.crsf');
+
+            return new RedirectResponse($this->generateUrl('admin_config'));
+        }
+
+        foreach ($ids as $id) {
+            $this->$method($id, $token);
+        }
+
+        return new RedirectResponse($this->generateUrl('admin_config'));
+
+    }
+
+    /**
+     * @Method({"POST", "GET"})
+     * @Route("/config/{id}/delete/{token}", name="admin_config_delete")
+     * @Secure(roles="ROLE_SUPER_ADMIN")
+     * @param string $id The document ID
+     * @param null $token
+     *
+     * @return array
+     *
+     * @throws \Symfony\Component\HttpKernel\Exception\NotFoundHttpException If document doesn't exists
+     */
+    public function deleteAction($id, $token = null)
+    {
+        $form       = $this->createDeleteForm($id);
+        $form->handleRequest($this->request);
+
+        if (null !== $token) {
+            $token = $this->csrf->isCsrfTokenValid('delete' . $id, $token);
+        }
+
+        if ($form->isValid() || true === $token) {
+
+            $document = $this->configManager->findPropertyById($id);
+
+            if (!$document) {
+                throw new ConfigNotFoundException();
+            }
+
+            $dm->removeAndFlush($document);
+
+            $this->session->getFlashbag()->add('success', 'black.bundle.config.success.config.admin.config.delete');
+
+        }
+
+        return new RedirectResponse($this->generateUrl('admin_config'));
+    }
+
+    /**
+     * @Method({"GET", "POST"})
+     * @Route("/{id}/edit", name="admin_config_edit")
+     * @Secure(roles="ROLE_SUPER_ADMIN")
+     * @Template()
+     *
+     * @param string $id The document ID
+     *
+     * @return array
+     *
+     * @throws \Symfony\Component\HttpKernel\Exception\NotFoundHttpException If document doesn't exists
+     */
+    public function editAction($id)
+    {
+        $document           = $this->configManager->findPropertyById($id);
+
+        if (!$document) {
+            throw new ConfigNotFoundException();
+        }
+
+        $process        = $this->handler->process($document);
+
+        if ($process) {
+            return $this->redirect($this->handler->getUrl());
+        }
+
+        return array(
+            'document'      => $document,
+            'form'          => $this->handler->getForm()->createView()
+        );
+    }
+
     /**
      * @Method("GET")
      * @Route("/index.html", name="admin_config")
@@ -41,9 +211,7 @@ class AdminConfigController extends Controller
      */
     public function indexAction()
     {
-        $dm = $this->getManager();
-
-        $rawDocuments  = $dm->findPropertiesBy(array('protected' => false));
+        $rawDocuments  = $this->configManager->findPropertiesBy(array('protected' => false));
         $csrf       = $this->get('form.csrf_provider');
 
         $documents = array();
@@ -71,142 +239,27 @@ class AdminConfigController extends Controller
      */
     public function newAction()
     {
-        $documentManager    = $this->getManager();
-        $document           = $documentManager->createInstance();
-
-        $formHandler        = $this->get('black_config.config.form.main.handler');
-        $process            = $formHandler->process($document);
+        $document           = $this->configManager->createInstance();
+        $process            = $this->handler->process($document);
 
         if ($process) {
-            return $this->redirect($formHandler->getUrl());
+            return $this->redirect($this->handler->getUrl());
         }
 
         return array(
             'document' => $document,
-            'form'     => $formHandler->getForm()->createView()
+            'form'     => $this->handler->getForm()->createView()
         );
 
 
     }
 
     /**
-     * @Method({"GET", "POST"})
-     * @Route("/{id}/edit", name="admin_config_edit")
-     * @Secure(roles="ROLE_SUPER_ADMIN")
-     * @Template()
+     * @param $id
      *
-     * @param string $id The document ID
-     *
-     * @return array
-     *
-     * @throws \Symfony\Component\HttpKernel\Exception\NotFoundHttpException If document doesn't exists
+     * @return mixed
      */
-    public function editAction($id)
-    {
-        $documentManager    = $this->getManager();
-        $document           = $documentManager->findPropertyById($id);
-
-        if (!$document) {
-            throw new ConfigNotFoundException();
-        }
-
-        $formHandler    = $this->get('black_config.config.form.main.handler');
-        $process        = $formHandler->process($document);
-
-        if ($process) {
-            return $this->redirect($formHandler->getUrl());
-        }
-
-        return array(
-            'document'      => $document,
-            'form'          => $formHandler->getForm()->createView()
-        );
-    }
-
-    /**
-     * @Method({"POST", "GET"})
-     * @Route("/config/{id}/delete/{token}", name="admin_config_delete")
-     * @Secure(roles="ROLE_SUPER_ADMIN")
-     * @param string $id The document ID
-     * @param null $token
-     *
-     * @return array
-     *
-     * @throws \Symfony\Component\HttpKernel\Exception\NotFoundHttpException If document doesn't exists
-     */
-    public function deleteAction($id, $token = null)
-    {
-        $form       = $this->createDeleteForm($id);
-        $request    = $this->getRequest();
-
-        $form->handleRequest($request);
-
-        if (null !== $token) {
-            $token = $this->get('form.csrf_provider')->isCsrfTokenValid('delete' . $id, $token);
-        }
-
-        if ($form->isValid() || true === $token) {
-
-            $dm = $this->getManager();
-            $document = $dm->findPropertyById($id);
-
-            if (!$document) {
-                throw new ConfigNotFoundException();
-            }
-
-            $dm->removeAndFlush($document);
-
-            $this->get('session')->getFlashbag()->add('success', 'black.bundle.config.success.config.admin.config.delete');
-
-        }
-
-        return $this->redirect($this->generateUrl('admin_config'));
-    }
-
-    /**
-     * @Method({"POST"})
-     * @Route("/config/batch", name="admin_config_batch")
-     *
-     * @return array
-     *
-     * @throws \Symfony\Component\Serializer\Exception\InvalidArgumentException If method does not exist
-     */
-    public function batchAction()
-    {
-        $request    = $this->getRequest();
-        $token      = $this->get('form.csrf_provider')->isCsrfTokenValid('batch', $request->get('token'));
-
-        if (!$ids = $request->get('ids')) {
-            $this->get('session')->getFlashbag()->add('error', 'black.bundle.config.error.config.admin.config.no.item');
-            return $this->redirect($this->generateUrl('admin_config'));
-        }
-
-        if (!$action = $request->get('batchAction')) {
-            $this->get('session')->getFlashBag()->add('error', 'black.bundle.config.error.config.admin.config.no.action');
-            return $this->redirect($this->generateUrl('admin_config'));
-        }
-
-        if (!method_exists($this, $method = $action . 'Action')) {
-            throw new Exception\InvalidArgumentException(
-                sprintf('You must create a "%s" method for action "%s"', $method, $action)
-            );
-        }
-
-        if (false === $token) {
-            $this->get('session')->getFlashBag()->add('error', 'black.bundle.config.error.config.admin.config.crsf');
-
-            return $this->redirect($this->generateUrl('admin_config'));
-        }
-
-        foreach ($ids as $id) {
-            $this->$method($id, $token);
-        }
-
-        return $this->redirect($this->generateUrl('admin_config'));
-
-    }
-
-    private function createDeleteForm($id)
+    protected function createDeleteForm($id)
     {
         $form = $this->createFormBuilder(array('id' => $id))
             ->add('id', 'hidden')
@@ -216,10 +269,11 @@ class AdminConfigController extends Controller
     }
 
     /**
-     * @return DocumentManager
+     * @param $url
+     * @param $parameters
      */
-    protected function getManager()
+    protected function generateUrl($url, $parameters)
     {
-        return $this->get('black_config.manager.config');
+        $this->router->generate($url, $parameters);
     }
 }
